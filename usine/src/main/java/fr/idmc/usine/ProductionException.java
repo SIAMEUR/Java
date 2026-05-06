2e082652659c337be07de17e8d82d5737e6a487a
SI AMEUR <hakimsiameur@gmail.com>
2026-05-07 01:33:23 +0200

    création usine + serveur avec les dépendances nécessaires

---
+++ usine/src/main/java/fr/idmc/usine/ProductionException.java
@@ -0,0 +1,23 @@
+package fr.idmc.usine;
+
+/**
+ * Exception levée lorsqu'une erreur survient pendant la fabrication des lunettes.
+ */
+public class ProductionException extends RuntimeException {
+
+    /**
+     * @param message description de l'erreur
+     */
+    public ProductionException(String message) {
+        super(message);
+    }
+
+    /**
+     * @param message description de l'erreur
+     * @param cause   exception d'origine
+     */
+    public ProductionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ usine/src/main/java/fr/idmc/usine/UsineImpl.java
@@ -0,0 +1,14 @@
+package fr.idmc.usine;
+
+import bernard_flou.Fabricateur;
+
+import java.util.List;
+import java.util.Map;
+// à développer
+public class UsineImpl implements Usine{
+
+    @Override
+    public List<Fabricateur.Lunette> produire(Map<Fabricateur.TypeLunette, Integer> typesLunettes) {
+        return List.of();
+    }
+}
