f9874b378b29375c510e465b484add45b42e700d
SI AMEUR <hakimsiameur@gmail.com>
2026-05-07 12:37:49 +0200

    feat: la squelette de l'usine avec constructeur et le log

--- usine/src/main/java/fr/idmc/usine/UsineImpl.java
+++ usine/src/main/java/fr/idmc/usine/UsineImpl.java
@@ -1,14 +1,35 @@
 package fr.idmc.usine;
 
 import bernard_flou.Fabricateur;
+import bernard_flou.Fabricateur.Lunette;
+import bernard_flou.Fabricateur.TypeLunette;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-// à développer
-public class UsineImpl implements Usine{
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class UsineImpl implements Usine {
+
+    private static final Logger logger = LoggerFactory.getLogger(UsineImpl.class);
+
+    private final Fabricateur fabricateur;
+    private final int capacite;
+    private final ExecutorService pool;
+
+    public UsineImpl(Fabricateur fabricateur) {
+        this.fabricateur = fabricateur;
+        this.capacite    = fabricateur.getCapacity();
+        this.pool        = Executors.newFixedThreadPool(capacite);
+        logger.info("Usine démarré avec capacite : {}", capacite);
+    }
 
     @Override
-    public List<Fabricateur.Lunette> produire(Map<Fabricateur.TypeLunette, Integer> typesLunettes) {
-        return List.of();
+    public List<Lunette> produire(Map<TypeLunette, Integer> typesLunettes) {
+        // TODO
+        return new ArrayList<>();
     }
 }
