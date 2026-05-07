19557670660bd23884d34245bdf2ae9bd519f900
SI AMEUR <hakimsiameur@gmail.com>
2026-05-07 15:38:54 +0200

    ajout des utils de l'usine + les classes nécessaire demande, slot

--- usine/src/main/java/fr/idmc/usine/UsineImpl.java
+++ usine/src/main/java/fr/idmc/usine/UsineImpl.java
@@ -32,4 +32,108 @@ public class UsineImpl implements Usine {
         // TODO
         return new ArrayList<>();
     }
+
+    /**
+     * extriare que les types dans chaque map (type:quantité)
+     * Exemple : {BlaBlaBla:2,Bananaaaa:1} en {BlaBlaBla,Bananaaaa}
+     * @param map la commande sous forme de Map
+     * @return la liste aplatie
+     */
+    private List<TypeLunette> aplatir(Map<TypeLunette, Integer> map) {
+        List<TypeLunette> liste = new ArrayList<>();
+        map.forEach((type, qnt) -> {
+            for (int i = 0; i < qnt; i++) {
+                liste.add(type);
+            }
+        });
+        return liste;
+    }
+
+    /**
+     * Découpe une selon la capacité
+     * Exemple : {BlaBlaBla,Bananaaaa} avec capacité de 1 -> {BlaBlaBla}, {Bananaaaa}
+     * @param liste  la liste a decouper
+     * @param capacité la taille max de chaque sous liste
+     * @param <T>    le type des élements
+     * @return la liste des sous listes
+     */
+    private <T> List<List<T>> decouper(List<T> liste, int capacité) {
+        List<List<T>> resultat = new ArrayList<>();
+        for (int i = 0; i < liste.size(); i += capacité) {
+            resultat.add(new ArrayList<>(liste.subList(i, Math.min(i + capacité, liste.size()))));
+        }
+        return resultat;
+    }
+    //Association entre un type de lunette à produire et la demande cliente qui l'a soumis
+    //c'est notre façon pour comment savoir à qui appartient la commande
+    private static class Slot {
+        private final TypeLunette type;
+        private final Demande demande;
+
+        Slot(TypeLunette type, Demande demande) {
+            this.type    = type;
+            this.demande = demande;
+        }
+
+        TypeLunette getType()    { return type; }
+        Demande     getDemande() { return demande; }
+    }
+    //pour savoir à qui la commande
+    /**
+     * @param lunette
+     * @param demande
+     */
+    private record LunetteEtDemande(Lunette lunette, Demande demande) {}
+
+    private static class Demande {
+
+        private final List<TypeLunette> types;
+        private final List<Lunette> lunettesRecues;
+        private final int quantiteTotale;
+        private ProductionException erreur = null;
+        private boolean termine = false;
+
+        Demande(List<TypeLunette> types) {
+            this.types          = types;
+            this.quantiteTotale = types.size();
+            this.lunettesRecues = new ArrayList<>();
+        }
+
+        List<TypeLunette> getTypes() { return types; }
+        /**
+         * @param lunette la lunette peoduite
+         */
+        synchronized void ajouterLunette(Lunette lunette) {
+            lunettesRecues.add(lunette);
+            if (lunettesRecues.size() >= quantiteTotale) {
+                termine = true;
+                notifyAll();
+            }
+        }
+        /**
+         * @param e
+         */
+        synchronized void signalerErreur(ProductionException e) {
+            this.erreur  = e;
+            this.termine = true;
+            notifyAll();
+        }
+        /**
+         * @return la liste des lunettes produites
+         * @throws ProductionException
+         */
+        synchronized List<Lunette> attendreResultat() {
+            while (!termine) {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new ProductionException("Attente interrompue", e);
+                }
+            }
+            if (erreur != null) throw erreur;
+            return new ArrayList<>(lunettesRecues);
+        }
+    }
+
 }
